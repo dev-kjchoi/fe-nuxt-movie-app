84dd6d0d863b6fb773d899ed45ceed9aed43f939
dev-kjchoi <kwangjiya@gmail.com>
2021-11-18 13:36:29 +0900

    add nuxt project

---
+++ server-middleware/movie.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+app.use(express.json());
+const { OMDB_API_KEY } = process.env;
+
+app.post("/", async (req, res) => {
+  const payload = req.body;
+  const { title, type, year, page, id } = payload;
+
+  console.log("OMDB_API_KEY: ", OMDB_API_KEY);
+  console.log("params: ", payload);
+
+  const url = id
+    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}&plot=full`
+    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
+
+  try {
+    const { data } = await axios.get(url);
+    if (data.Error) {
+      res.status(400).json(data.Error);
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(error.response.status).json(error.message);
+  }
+});
+
+module.exports = app;
